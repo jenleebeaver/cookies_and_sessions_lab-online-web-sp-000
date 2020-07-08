442a3042d1b88c56f68714c62ee31a3316f355c5
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-07-08 19:42:24 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'products#index' #this is
+  root 'products#index' # means that root path '/' will go to our products controller, index method
   post '/' => 'products#add'
 end
